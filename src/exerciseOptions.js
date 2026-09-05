6f2e9ba028d699d0efc5386162269ce8e873ebbd
justinmhudson <jhud2010@gmail.com>
2026-09-05 12:35:30 -0700

    Cleaned up names, moved biceps curl to LB

--- src/exerciseOptions.js
+++ src/exerciseOptions.js
@@ -3,11 +3,13 @@
 
 export const EXERCISES_BY_CATEGORY = {
   'Cardio': ['Treadmill', 'Stair Master', 'Stationary Bike'],
-  'Upper Body': ['Chest Press', 'Pulldown', 'Row', 'Shoulder Press', 'Bicep Curl', 'Tricep Press', 'Triceps Extension'],
-  'Lower Body': ['Squat', 'RDL', 'DB Calf Raises', 'Abdominal', 'DB RDL', 'DB Squat', 'Calf Extension'],
+  'Upper Body': ['Chest Press', 'Pulldown', 'Row', 'Shoulder Press', 'Triceps Press'],
+  'Lower Body': ['Squat', 'RDL', 'DB Calf Raises', 'Abdominal', 'Biceps Curl'],
   'Other': ['Yoga', 'Balance'],
 };
 
+// 'Bicep Curl'
+// 'DB RDL', 'DB Squat', 'Calf Extension'
 // , 'Leg Press', 'Back Extension', 'Leg Curl'
 
 export const CATEGORIES = Object.keys(EXERCISES_BY_CATEGORY);
